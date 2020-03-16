5f0e514cbdb13c8f3953e476c7df70cd22d944e6
kripson <sankit.jobs@gmail.com>
2020-03-16 23:16:21 +1000

    Made navbar responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React,{useState} from "react";
 import './Nav.scss';
 import Linkedin from '../../assets/linkedinactive.png';
 import Facebook from '../../assets/facebook.png';
+import Hamburger from '../../assets/menu.png';
+import Close from '../../assets/close.png';
 
 
 const Nav = () =>
 {
+    var hamburgerbutton = "";
+    const [hamburger,sethamburger] = useState('-20em');
+
+
+    if(hamburger === '-20em')
+    {
+         hamburgerbutton =<img src={Hamburger} alt = "Hamburger Button"/>;
+
+    }
+    else
+    {
+         hamburgerbutton = <img src={Close} alt = "Close Button"/>;
+    }
+
+
     return(
         <div className="Nav">
+            <div className="hamburger" onClick={()=>{
+                if(hamburger === "0")
+                {
+                    sethamburger('-20em');
+                }
+                else
+                {
+                    sethamburger('0');
+                }
+
+            }}>
+                {hamburgerbutton}
+            </div>
+            <div className="hamburgermenu" style = {{top:hamburger}}>
+                <ul>
+                    <li>
+                        <a onClick={()=>sethamburger('-20em')} href="#homecontainer">Home</a>
+                    </li>
+                    <li>
+                        <a onClick={()=>sethamburger('-20em')} href="#aboutmecontainer">About Me</a>
+                    </li>
+                    <li>
+                        <a onClick={()=>sethamburger('-20em')} href="#skillscontainer">Skills</a>
+                    </li>
+                    <li>
+                        <a onClick={()=>sethamburger('-20em')} href="#projectscontainer"> Projects</a>
+                    </li>
+                    <li>
+                        <a onClick={()=>sethamburger('-20em')} href="#contactscontainer">Contact</a>
+                    </li>
+                </ul>
+            </div>
             <div className="NavigationLinks">
                <ul>
                    <li>
