ecdc95523d62b623d2793891212efcc11d136bcd
kripson <sankit.jobs@gmail.com>
2020-03-21 23:55:44 +1000

    fixed projects array

--- src/components/ProjectFrame/ProjectFile.js
+++ src/components/ProjectFrame/ProjectFile.js
@@ -1,6 +1,12 @@
 import React,{useState} from "react";
 import './ProjectFrame.scss';
 import ImageLoader from '../../assets/imageLoading.gif';
+import underconstruction from '../../assets/underconstruction.png';
+import ReactIcon from '../../assets/react.png';
+import Angular from '../../assets/angular.png';
+import Node from '../../assets/nodejs.png';
+import Laravel from '../../assets/laravel.png';
+
 
 
 
@@ -9,6 +15,28 @@ const ProjectFrame = ({project}) =>
 
     const [loaded,setLoaded] = useState(false);
     var projectcontent = '';
+
+    const imageMap = {
+        react: ReactIcon,
+        angular: Angular,
+        node:Node,
+        laravel:Laravel
+    };
+
+    const toolsimages = project.tools.map((tool)=> <img src = {imageMap[tool]} alt = 'Toolsimages'/>);
+
+    var construction = "";
+
+    if(!project.ready)
+    {
+        construction = <div className="underconstruction"><div className = "toolsused"><span>Tools used:  </span>{toolsimages}</div><span>Under Construction</span><img className="underconstructionicon" src={underconstruction} alt = "underconstruction" /></div>
+    }
+
+
+
+
+
+
 
     if(loaded) {
         projectcontent = <a href = {project.websiteUrl ? project.websiteUrl : ""}><img className = "projectimage" src={project.imageUrl}/></a>
@@ -25,6 +53,7 @@ const ProjectFrame = ({project}) =>
     },3000);
 
     return(<div style={{animationDelay: `${project.id / 9}s`}} className="project">
+        {construction}
         {projectcontent}
 
     </div>)
--- src/pages/projectsarray.js
+++ src/pages/projectsarray.js
@@ -1,51 +1,48 @@
 export const projectsarray = [
     {
         id:1,
-        name:'Ruby',
-        imageUrl:require("../assets/ruby.jpeg"),
-    },
-    {
-        id:2,
         name:'Clothingapp',
         imageUrl:require("../assets/clothingapp.jpeg"),
-        websiteUrl: 'https://kripson-store.herokuapp.com/'
+        websiteUrl: 'https://kripson-store.herokuapp.com/',
+        ready: true,
+        tools:['react','node']
 
     },
     {
+        id:2,
+        name:'DrivingSchool',
+        imageUrl:require("../assets/Drivingschool.jpeg"),
+        websiteUrl: 'https://drivingform.herokuapp.com/',
+        ready:true,
+        tools:['react','node']
+    },
+    {
         id:3,
-        name:'FaceDetection',
-        imageUrl:require("../assets/facedetection.jpeg")
+        name:'Ruby',
+        imageUrl:require("../assets/ruby.jpeg"),
+        ready:false,
+        tools:['react','node']
     },
     {
         id:4,
-        name:'Ruby',
-        imageUrl:require("../assets/ruby.jpeg")
+        name:'Bootcamp',
+        imageUrl:require("../assets/Bootcamp.png"),
+        ready:false,
+        tools:['laravel']
     },
     {
         id:5,
-        name:'Clothingapp',
-        imageUrl:require("../assets/clothingapp.jpeg"),
-        websiteUrl: 'https://kripson-store.herokuapp.com/'
+        name:'Eportfolio',
+        imageUrl:require("../assets/Eportfolio.jpeg"),
+        websiteUrl: 'https://kripson-portfolio.herokuapp.com/',
+        ready:true,
+        tools:['react']
     },
     {
         id:6,
         name:'FaceDetection',
-        imageUrl:require("../assets/facedetection.jpeg")
-    },
-    {
-        id:7,
-        name:'Ruby',
-        imageUrl:require("../assets/ruby.jpeg")
-    },
-    {
-        id:8,
-        name:'Clothingapp',
-        imageUrl:require("../assets/clothingapp.jpeg"),
-        websiteUrl: 'https://kripson-store.herokuapp.com/'
-    },
-    {
-        id:9,
-        name:'FaceDetection',
-        imageUrl:require("../assets/facedetection.jpeg")
+        imageUrl:require("../assets/facedetection.jpeg"),
+        ready:false,
+        tools:['react','node']
     }
-]
+];
